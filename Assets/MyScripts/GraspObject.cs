3fd8c2173e3fd717698e1d715607bc9a2cc8580c
Gauri Desai <91729688+GauriDesai11@users.noreply.github.com>
2025-02-25 13:28:36 +0000

    trying to add networked objects and assign IDs to users

--- Assets/MyScripts/GraspObject.cs
+++ Assets/MyScripts/GraspObject.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Ubiq.Messaging;
 using Ubiq.Rooms;
-using UnityEngine;
 //#if XRI_3_0_7_OR_NEWER
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
@@ -13,21 +15,27 @@ public class GraspObject : MonoBehaviour
     private XRSimpleInteractable interactable;
     private RoomClient roomClient;
     private bool isHeld = false;
-    private bool owner = false; // Track if this player owns the object
-    private string assignedUserID; // The user ID allowed to interact with this object
+    private bool owner = false;
+    private string assignedUserID;
 
-    public string initialUserID; // Set this in Inspector to define ownership. Use "-1" for shared objects.
+    public string initialUserID; // Set in Inspector. "-1" means any user can grab it.
+
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
 
     private void Start()
     {
         context = NetworkScene.Register(this);
         interactable = GetComponent<XRSimpleInteractable>();
         roomClient = RoomClient.Find(this);
-
-        assignedUserID = initialUserID; // Assign ownership from inspector
+        assignedUserID = initialUserID;
 
         interactable.selectEntered.AddListener(OnGrab);
         interactable.selectExited.AddListener(OnRelease);
+
+        UnityEngine.Debug.Log("[GraspObject] assigned ID = " + assignedUserID);
+        
+        UnityEngine.Debug.Log("[GraspObject] current ID = " + UserIDManager.Instance.GetUserID(roomClient.Me.uuid));
     }
 
     private void OnDestroy()
@@ -36,50 +44,110 @@ public class GraspObject : MonoBehaviour
         interactable.selectExited.RemoveListener(OnRelease);
     }
 
+    private void Update()
+    {
+        string currentUserID = UserIDManager.Instance.GetUserID(roomClient.Me.uuid);
+        // If this player is the owner, send updates if the object moves
+        if (owner && (transform.position != lastPosition || transform.rotation != lastRotation))
+        {
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+
+            context.SendJson(new Message(currentUserID, true, lastPosition, lastRotation));
+        }
+    }
+
     private void OnGrab(SelectEnterEventArgs eventArgs)
     {
         string currentUserID = UserIDManager.Instance.GetUserID(roomClient.Me.uuid);
 
-        // If the assignedUserID is "-1", allow anyone to grab it
+        // Allow only the assigned user or any user if "-1"
         if (assignedUserID != "-1" && currentUserID != assignedUserID)
         {
-            Debug.Log($"User {currentUserID} is NOT allowed to grab this object (Owned by {assignedUserID})");
+            UnityEngine.Debug.Log($"User {currentUserID} is NOT allowed to grab this object (Owned by {assignedUserID})");
             return;
         }
 
         owner = true;
         isHeld = true;
-        context.SendJson(new OwnershipMessage(currentUserID, true));
+
+        // Notify the network about ownership change
+        context.SendJson(new Message(currentUserID, true, lastPosition, lastRotation));
     }
 
     private void OnRelease(SelectExitEventArgs eventArgs)
     {
         owner = false;
         isHeld = false;
-        context.SendJson(new OwnershipMessage(assignedUserID, false));
+
+        // Notify network that this player released the object
+        context.SendJson(new Message(assignedUserID, false, lastPosition, lastRotation));
     }
 
     public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
     {
-        var data = message.FromJson<OwnershipMessage>();
+        var data = message.FromJson<Message>();
 
-        if (!owner) // Only update if this instance is not the owner
+        if (!owner) // Only update if not the owner
         {
             assignedUserID = data.userID;
             isHeld = data.held;
+            transform.position = data.position;
+            transform.rotation = data.rotation;
         }
+
+        /*
+        if (message.Tag == "ownership")
+        {
+            var data = message.FromJson<OwnershipMessage>();
+
+            if (!owner) // Only update if not the owner
+            {
+                assignedUserID = data.userID;
+                isHeld = data.held;
+            }
+        }
+        else if (message.Tag == "position")
+        {
+            var data = message.FromJson<ObjectUpdateMessage>();
+
+            if (!owner) // Only non-owners should update
+            {
+                transform.position = data.position;
+                transform.rotation = data.rotation;
+            }
+        }
+        */
     }
 
-    private struct OwnershipMessage
+    private struct Message
     {
         public string userID;
         public bool held;
+        public Vector3 position;
+        public Quaternion rotation;
 
-        public OwnershipMessage(string userID, bool held)
+        public Message(string userID, bool held, Vector3 position, Quaternion rotation)
         {
             this.userID = userID;
             this.held = held;
+            this.position = position;
+            this.rotation = rotation;
         }
     }
+
+    /*
+    private struct ObjectUpdateMessage
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public ObjectUpdateMessage(Vector3 position, Quaternion rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
+    }
+    */
 //#endif
 }
