6db810fd43dba40a4ae3eb85f7a98620870c6918
Gauri Desai <91729688+GauriDesai11@users.noreply.github.com>
2025-03-21 17:50:39 +0000

    new scale avatar script

--- Assets/MyScripts/ScaleAvatar.cs
+++ Assets/MyScripts/ScaleAvatar.cs
@@ -1,30 +1,34 @@
 using System.Diagnostics;
+//using System.Runtime.Remoting.Contexts;
 using UnityEngine;
 using UnityEngine.UI;
+using Ubiq.Messaging;
 
 public class ScaleAvatar : MonoBehaviour
 {
     [Header("Objects to Scale")]
     [Tooltip("First small object inside the main object")]
-    public Transform object1;
+    public Transform head;
 
     [Tooltip("Second small object inside the main object")]
-    public Transform object2;
+    public Transform torso;
 
-    [Header("Sliders for Object 1")]
+    [Header("Sliders for Head")]
     public Slider object1_X_Slider;
     public Slider object1_Y_Slider;
     public Slider object1_Z_Slider;
 
-    [Header("Sliders for Object 2")]
+    [Header("Sliders for Torso")]
     public Slider object2_X_Slider;
     public Slider object2_Y_Slider;
     public Slider object2_Z_Slider;
 
+    private NetworkContext context;
+
     private void Start()
     {
         // Ensure objects exist
-        if (object1 == null || object2 == null)
+        if (head == null || torso == null)
         {
             UnityEngine.Debug.LogError("ScaleController: Objects not assigned!");
             return;
@@ -34,43 +38,118 @@ public class ScaleAvatar : MonoBehaviour
         SetSliderDefaults();
 
          //Add listeners to sliders for real-time scale updates
-        object1_X_Slider.onValueChanged.AddListener(value => UpdateScale(object1, value, 'x'));
-        object1_Y_Slider.onValueChanged.AddListener(value => UpdateScale(object1, value, 'y'));
-        object1_Z_Slider.onValueChanged.AddListener(value => UpdateScale(object1, value, 'z'));
+        object1_X_Slider.onValueChanged.AddListener(value => UpdateScale(1, value, 'x'));
+        object1_Y_Slider.onValueChanged.AddListener(value => UpdateScale(1, value, 'y'));
+        object1_Z_Slider.onValueChanged.AddListener(value => UpdateScale(1, value, 'z'));
 
-        object2_X_Slider.onValueChanged.AddListener(value => UpdateScale(object2, value, 'x'));
-        object2_Y_Slider.onValueChanged.AddListener(value => UpdateScale(object2, value, 'y'));
-        object2_Z_Slider.onValueChanged.AddListener(value => UpdateScale(object2, value, 'z'));
+        object2_X_Slider.onValueChanged.AddListener(value => UpdateScale(2, value, 'x'));
+        object2_Y_Slider.onValueChanged.AddListener(value => UpdateScale(2, value, 'y'));
+        object2_Z_Slider.onValueChanged.AddListener(value => UpdateScale(2, value, 'z'));
     }
 
     private void SetSliderDefaults()
     {
-        object1_X_Slider.value = object1.localScale.x;
-        object1_Y_Slider.value = object1.localScale.y;
-        object1_Z_Slider.value = object1.localScale.z;
+        object1_X_Slider.value = head.localScale.x;
+        object1_Y_Slider.value = head.localScale.y;
+        object1_Z_Slider.value = head.localScale.z;
 
-        object2_X_Slider.value = object2.localScale.x;
-        object2_Y_Slider.value = object2.localScale.y;
-        object2_Z_Slider.value = object2.localScale.z;
+        object2_X_Slider.value = torso.localScale.x;
+        object2_Y_Slider.value = torso.localScale.y;
+        object2_Z_Slider.value = torso.localScale.z;
     }
 
-    private void UpdateScale(Transform obj, float value, char axis)
+    private void UpdateScale(float obj_id, float value, char axis)
     {
-        Vector3 newScale = obj.localScale;
+        Vector3 newScale = new Vector3(0,0,0);
+        if (obj_id == 1)
+        {
+            newScale = head.localScale;
+        }else
+        {
+            newScale = torso.localScale;
+        }
+
+            switch (axis)
+            {
+                case 'x':
+                    newScale.x = value;
+                    
+                    break;
+                case 'y':
+                    newScale.y = value;
+
+                    break;
+                case 'z':
+                    newScale.z = value;
+
+                    break;
+            }
+
+        //obj.localScale = newScale;
+        if (obj_id == 1)
+        {
+            head.localScale = newScale;
+        }
+        else
+        {
+            torso.localScale = newScale;
+        }
+
+        context.SendJson(new Message() { objID = obj_id, val = value, ax = axis });
+
+        UnityEngine.Debug.Log("sending scaled avatar");
+    }
+
+    private struct Message
+    {
+        public float objID;
+        public float val;
+        public char ax;
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        // Parse the message
+        var m = message.FromJson<Message>();
+
+        float obj_id = m.objID;
+        float value = m.val;
+        char axis = m.ax;
+
+        Vector3 newScale = new Vector3(0, 0, 0);
+        if (obj_id == 1)
+        {
+            newScale = head.localScale;
+        }
+        else
+        {
+            newScale = torso.localScale;
+        }
 
         switch (axis)
         {
             case 'x':
                 newScale.x = value;
+
                 break;
             case 'y':
                 newScale.y = value;
+
                 break;
             case 'z':
                 newScale.z = value;
+
                 break;
         }
 
-        obj.localScale = newScale;
+        //obj.localScale = newScale;
+        if (obj_id == 1)
+        {
+            head.localScale = newScale;
+        }
+        else
+        {
+            torso.localScale = newScale;
+        }
     }
 }
