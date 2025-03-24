b1d7a4fd8cc1cd3a724e97809d7e61b3455e1a82
chidinmaaa <92586177+chidinmaaa@users.noreply.github.com>
2025-03-24 23:00:33 +0000

    narration only plays when both players are present

--- Assets/NarrationHandler.cs
+++ Assets/NarrationHandler.cs
@@ -12,38 +12,48 @@ public class NarrationHandler : MonoBehaviour
     public float pause;
 
     private SimpleSpeechController controller;
+    private GameObject avatarManager;
 
     void Start()
     {
-        controller = GetComponent<SimpleSpeechController>();
         pause = 1.0f;
+        controller = GetComponent<SimpleSpeechController>();
+        avatarManager = GameObject.Find("Avatar Manager");
+
     }
 
     void Update()
     {
-        if (in_entrance){
-            controller.Speak("entrance", pause);
-            in_entrance = false;
-        }
-        if (in_body_lab){
-            //controller.Speak("body_lab", pause);
-            in_body_lab = false;
-        }
-        if (in_style_station){ 
-            controller.Speak("style_station",pause);
-            in_style_station = false;
-        }
-        if (in_accessory_studio){
-            controller.Speak("accessory_studio",pause);
-            in_accessory_studio = false;
-        }
-        if (in_final_room){ 
-            controller.Speak("final_room",pause);
-            in_final_room = false;
-        }
-        if (on_conveyor){
-            controller.Speak("belt", pause);
-            on_conveyor = false;
+        if (avatarManager.transform.childCount > 1)
+        {
+            if (in_entrance){
+                controller.Speak("entrance", pause);
+                in_entrance = false;
+            }
+            if (in_body_lab){
+                //controller.Speak("body_lab", pause);
+                in_body_lab = false;
+            }
+            if (in_style_station)
+            {
+                controller.Speak("style_station", pause);
+                in_style_station = false;
+            }
+            if (in_accessory_studio)
+            {
+                controller.Speak("accessory_studio", pause);
+                in_accessory_studio = false;
+            }
+            if (in_final_room)
+            {
+                controller.Speak("final_room", pause);
+                in_final_room = false;
+            }
+            if (on_conveyor)
+            {
+                controller.Speak("belt", pause);
+                on_conveyor = false;
+            }
         }
     }
 }
