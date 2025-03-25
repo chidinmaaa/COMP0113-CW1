f3724d8d20425fa489b0898cf149ef1cfff9829c
ofirfrancis <ofir.francis@gmail.com>
2025-03-25 16:30:14 +0000

    Accessory respawning

--- Assets/MyScripts/Respawn_Accessories.cs
+++ Assets/MyScripts/Respawn_Accessories.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using Unity.Mathematics;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -8,46 +10,73 @@ public class Respawn_Accessories : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private GameObject[] objects;
-    private Pose[] originalPositions;
+    //private Pose[] originalPositions;
+    private Quaternion[] originalRotations;
+    private Vector3[] originalPositions;
+    private String[] names;
 
     void Start()
     {
         objects = GameObject.FindGameObjectsWithTag("Accessory");
-        originalPositions = new Pose[objects.Length];
+        //GameObject[] prefabs = Resources.LoadAll<GameObject>("Accessories");
+
+        //originalPositions = new Pose[objects.Length];
+        originalPositions = new Vector3[objects.Length];
+        originalRotations = new Quaternion[objects.Length];
+        names = new String[objects.Length];
         int index = 0;
         foreach (GameObject obj in objects)
         {
             if (obj == null) continue;
-            originalPositions[index] = obj.transform.GetWorldPose();
+            //originalPositions[index] = obj.transform.GetWorldPose();
+            originalPositions[index] = obj.transform.position;
+            originalRotations[index] = obj.transform.rotation;
+            names[index] = obj.name;
             index++;
         }
+        Debug.Log(names);
 
-        
+
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
+        objects = GameObject.FindGameObjectsWithTag("Accessory");
+        //Debug.Log("Accesory collider " + other.gameObject.name);
         for (int i = 0; i < objects.Length; i++)
         {
             GameObject obj = objects[i];
-            if (obj != null)
-            {
-                obj.transform.SetParent(null);
-                obj.transform.SetWorldPose(originalPositions[i]);
+            //Vector3 pos = obj.transform.position;
+            //quaternion quaternion = obj.transform.rotation;
+            //String name = obj.name;
+            Destroy(obj);
+            GameObject prefab = Resources.Load<GameObject>("Accessories/" + names[i]);
+            Instantiate(prefab, originalPositions[i], originalRotations[i]);
+            //if (obj != null)
+            //{
+            //    obj.transform.SetParent(null);
+            //    obj.transform.SetWorldPose(originalPositions[i]);
 
-                if (obj.GetComponent<Rigidbody>() == null)
-                {
-                    Rigidbody rb = obj.AddComponent<Rigidbody>();
-                    rb.useGravity = false;
-                }
+            //    if (obj.GetComponent<Rigidbody>() == null)
+            //    {
+            //        Rigidbody rb = obj.AddComponent<Rigidbody>();
+            //        rb.useGravity = false;
+            //    }
 
-                if (obj.GetComponent<XRGrabInteractable>() == null)
-                {
-                    obj.AddComponent<XRGrabInteractable>();
-                }
+            //    if (obj.GetComponent<XRGrabInteractable>() == null)
+            //    {
+            //        obj.AddComponent<XRGrabInteractable>();
+            //    }
 
-            }
+            //}
         }
+
+        //Debug.Log(prefabs.Length);
+
+        //foreach (GameObject obj in prefabs) {
+        //    Debug.Log(obj.name);
+        //    Instantiate(obj);
+        //}
     }
 }
